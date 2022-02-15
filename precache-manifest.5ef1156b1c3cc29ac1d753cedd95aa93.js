7bfade32cf42a9e404bb1e7610bc74045d291bdb
HBMATT <hannahbmatt@gmail.com>
2022-02-15 16:16:36 -0600

    Updates

---
+++ precache-manifest.5ef1156b1c3cc29ac1d753cedd95aa93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15df524f8546cca30d3096b8eb46d5f",
+    "url": "/morphing-shapes-starter/index.html"
+  },
+  {
+    "revision": "1a00545eff8e1e870432",
+    "url": "/morphing-shapes-starter/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "133e3d9e4998fde5159b",
+    "url": "/morphing-shapes-starter/static/css/main.db3babfd.chunk.css"
+  },
+  {
+    "revision": "1a00545eff8e1e870432",
+    "url": "/morphing-shapes-starter/static/js/2.ebc453d4.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/morphing-shapes-starter/static/js/2.ebc453d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133e3d9e4998fde5159b",
+    "url": "/morphing-shapes-starter/static/js/main.b0c0c643.chunk.js"
+  },
+  {
+    "revision": "d70bba6784dc8b4aa592",
+    "url": "/morphing-shapes-starter/static/js/runtime-main.99e37303.js"
+  }
+]);
